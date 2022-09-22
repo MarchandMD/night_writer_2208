5101c9c11946bf9703700c643eac663b8feada57
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-22 17:45:29 -0600

    Pass first test for #process

--- lib/message_processor.rb
+++ lib/message_processor.rb
@@ -1,2 +1,11 @@
 class MessageProcessor
+  def initialize; end
+
+  def process
+    if ARGV.length > 1
+      puts "Created #{ARGV[1]} containing 256 characters"
+    else
+      puts "Created 'braille.txt' containing 256 characters"
+    end
+  end
 end
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,9 +1,5 @@
 require './lib/message_processor'
 
-if ARGV.length > 1
-  puts "Created #{ARGV[1]} containing 256 characters"
-else
-  puts "Created 'braille.txt' containing 256 characters"
-end
+
 
 MessageProcessor.new.process
