c679d59a9610d3771769f576de591ff8aebc89a3
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-22 17:44:29 -0600

    Add test for #process

--- spec/message_processor_spec.rb
+++ spec/message_processor_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe MessageProcessor do
     msg = MessageProcessor.new
     expect(msg).to be_a MessageProcessor
   end
+
+  describe '#process' do
+    it 'puts a line of text to the terminal, without newline' do
+      msg = MessageProcessor.new
+      expect{ msg.process }.to output.to_stdout
+    end
+  end
 end
