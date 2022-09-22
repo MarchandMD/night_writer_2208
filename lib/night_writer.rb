312fa4182ca5a259d178582f08c91da8e3682a5a
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-22 17:39:03 -0600

    Modify night_writer to act as a runner, and add message_processr file and spec

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,9 @@
-class NightWriter
-  def message
-    print "Created 'braille.txt' containing 256 characters"
-  end
+require './lib/message_processor'
+
+if ARGV.length > 1
+  puts "Created #{ARGV[1]} containing 256 characters"
+else
+  puts "Created 'braille.txt' containing 256 characters"
 end
+
+MessageProcessor.new.process
