cac03652ad10525534e8e853a58381d4d106cbe5
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-22 20:42:05 -0600

    Test dictionary and #to_braille

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Dictionary do
     dictionary = Dictionary.new
     expect(dictionary).to be_a Dictionary
   end
+
+  describe '#to_braille' do
+    it 'converts a string letter to braille' do
+      dictionary = Dictionary.new
+      expect(dictionary.to_braille('a')).to eq([['0', '.'], ['.', '.'], ['.', '.']])
+      expect(dictionary.to_braille('b')).to eq([['0', '.'], ['0', '.'], ['.', '.']])
+      expect(dictionary.to_braille('z')).to eq([['0', '.'], ['.', '0'], ['0', '0']])
+    end
+  end
 end
