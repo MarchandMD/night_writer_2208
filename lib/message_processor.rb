aa2c218ba47d2fe0362b867a4bacc64f35aa1478
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-22 22:44:08 -0600

    Add attr_readers for both processors

--- lib/message_processor.rb
+++ lib/message_processor.rb
@@ -2,19 +2,23 @@ require './lib/file_processor'
 
 class MessageProcessor
 
-  attr_reader :input
+  attr_reader :input, :output, :contents
 
-  def initialize
-    @input = FileProcessor.new
+  def initialize(input = { input: ARGV[0], output: ARGV[1] })
+    @input = input.fetch(:input)
+    @output = input.fetch(:output)
+    @contents = nil
   end
 
   def process
     if ARGV.length > 1
-      file_to_be_read = @input.open_input(ARGV[0])
-      contents = file_to_be_read.read
+      file_to_be_read = @input
+      @contents = file_to_be_read.read
       puts "Created #{ARGV[1]} containing #{contents.length} characters"
     else
       puts "Created 'braille.txt' containing 256 characters"
     end
   end
+
+
 end
--- spec/message_processor_spec.rb
+++ spec/message_processor_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe MessageProcessor do
-  msg = MessageProcessor.new
+  input = {
+    input: 'sample.txt',
+    output: 'braille.txt'
+  }
+  msg = MessageProcessor.new(input)
 
   it 'exists' do
     expect(msg).to be_a MessageProcessor
   end
 
-  it 'has @input' do
-    expect(msg.input).to be_a FileProcessor
+  it 'has @input to be nil by default' do
+    expect(msg.input).to eq('sample.txt')
+  end
+
+  it 'has @output' do
+    expect(msg.output).to eq('braille.txt')
   end
 
   it 'has @contents set to nil by default' do
@@ -16,12 +24,15 @@ RSpec.describe MessageProcessor do
   end
 
   describe '#process' do
+    input = {
+    input: 'sample.txt',
+    output: 'braille.txt'
+  }
+    msg = MessageProcessor.new(input)
     it 'puts a line of text to the terminal' do
-      msg = MessageProcessor.new
       expect { msg.process }.to output.to_stdout
     end
     it 'puts a specific line of text to the terminal' do
-      msg = MessageProcessor.new
       expect { msg.process }.to output("Created 'braille.txt' containing 256 characters\n").to_stdout
     end
   end
