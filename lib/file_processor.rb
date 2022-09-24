d20afe8840d5c21a3172323c5b4beffc6763d3b3
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-24 08:50:52 -0600

    Format File_processor

--- lib/file_processor.rb
+++ lib/file_processor.rb
@@ -1,8 +1,8 @@
 class FileProcessor
-   attr_reader :input, :output, :output_filename, :contents
+  attr_reader :input, :output, :output_filename, :contents
 
-   def initialize(input = {input: ARGV[0], output: ARGV[1]} )
-    @input =  input.fetch(:input)
+  def initialize(input = { input: ARGV[0], output: ARGV[1] })
+    @input = input.fetch(:input)
     @output_filename = input.fetch(:output)
     @output = write_file
   end
@@ -10,5 +10,4 @@ class FileProcessor
   def write_file
     @output = File.open(@output_filename, 'w')
   end
-
 end
