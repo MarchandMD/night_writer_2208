da5d249ad230c40408208b70d5f567531ea49587
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-24 10:52:02 -0600

    Write test for @braille_array

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,19 @@
 require './lib/dictionary'
 require './lib/file_processor'
 
+class NightWriter
+  attr_reader :file_handler, :dictionary, :braille_array, :braille_data
+
+  def initialize(file_handler = FileProcessor.new)
+    @file_handler = file_handler
+    @dictionary = Dictionary.new
+    @braille_array = convert_to_braille
+    @braille_data = ['', '', '']
+  end
+
+  def convert_to_braille
+    @file_handler.separate_letters.map do |x|
+      @dictionary.to_braille(x)
+    end
+  end
+end
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe NightWriter do
-  night_writer = NightWriter.new
+  file_processor = FileProcessor.new({ input: 'sample.txt', output: 'braille.txt' })
+  night_writer = NightWriter.new(file_processor)
   it 'exists' do
     expect(night_writer).to be_a NightWriter
+  end
+
+  it 'has @file_handler, @dictionary, @braille_data, @braille_array' do
+    expect(night_writer.file_handler).to be_a FileProcessor
+    expect(night_writer.dictionary).to be_a Dictionary
+  end
+
+  describe '#convert_to_braille' do
+    file_processor = FileProcessor.new({ input: 'sample.txt', output: 'braille.txt' })
+    night_writer = NightWriter.new(file_processor)
+    it 'converts a letter to braille' do
+      expect(night_writer.convert_to_braille).to eq([[["0", "."], [".", "."], [".", "."]]])
+    end
   end
 end
