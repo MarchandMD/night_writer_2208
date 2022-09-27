485a0939b9a7050229d0f080ec524b050720d43c
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-27 15:32:36 -0600

    refactor night_reader and night_writer instance variable names

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -3,12 +3,12 @@ require './lib/file_processor'
 
 class NightReader
   attr_reader :file_handler, :dictionary, :message, :message_container, :characters_in_message
-  attr_accessor :array_from_parsed_braille, :input_data
+  attr_accessor :array_of_braille_pairs, :input_data
 
-  def initialize(file_handler = FileProcessor.new, dictionary = Dictionary.new, array_from_parsed_braille = nil, message = '', message_container = [], input_data = nil)
+  def initialize(file_handler = FileProcessor.new, dictionary = Dictionary.new, array_of_braille_pairs = nil, message = '', message_container = [], input_data = nil)
     @file_handler = file_handler
     @dictionary = dictionary
-    @array_from_parsed_braille = array_from_parsed_braille
+    @array_of_braille_pairs = array_of_braille_pairs
     @message = message
     @characters_in_message = 0
     @message_container = message_container
@@ -19,7 +19,7 @@ class NightReader
     @dictionary = @dictionary.braille.invert.transform_keys(&:flatten)
   end
 
-  def character_containers_needed(raw_braille_pairs = @array_from_parsed_braille)
+  def character_containers_needed(raw_braille_pairs = @array_of_braille_pairs)
     @characters_in_message = raw_braille_pairs.flatten.length / 6
   end
 
@@ -29,7 +29,7 @@ class NightReader
     end
   end
 
-  def fill_character_containers(a_single_row = [["0", "."], [".", "."], [".", "."]])
+  def fill_character_containers(a_single_row = [['0', '.'], ['.', '.'], ['.', '.']])
     @message_container.cycle(3) do |character_container|
       character_container << a_single_row.shift
     end
@@ -37,8 +37,8 @@ class NightReader
 
   def create_braille_pairs
     @input_data = @file_handler.read_and_remove_newline_characters
-    @array_from_parsed_braille = @input_data.scan(/../)
-    @array_from_parsed_braille = @array_from_parsed_braille.map { |braille_pair| braille_pair.split('') }
+    @array_of_braille_pairs = @input_data.scan(/../)
+    @array_of_braille_pairs = @array_of_braille_pairs.map { |braille_pair| braille_pair.split('') }
   end
 
   def flatten_message
@@ -58,9 +58,9 @@ class NightReader
   def process
     convert_dictionary
     create_braille_pairs
-    until @array_from_parsed_braille.count.zero?
+    until @array_of_braille_pairs.count.zero?
       @message_container = []
-      raw_braille_pairs = @array_from_parsed_braille.slice!(0, 120)
+      raw_braille_pairs = @array_of_braille_pairs.slice!(0, 120)
       character_containers_needed(raw_braille_pairs)
       create_character_containers
       fill_character_containers(raw_braille_pairs)
@@ -72,6 +72,4 @@ class NightReader
     @file_handler.output.write(@message)
     print_a_confirmation_message
   end
-
 end
-
--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'pry'
 
 RSpec.describe NightReader do
-  file_processor = FileProcessor.new({ input: 'braille.txt', output: 'sample.txt' })
+  file_processor = FileProcessor.new({ input: 'braille.txt', output: 'message.txt' })
   night_reader = NightReader.new(file_processor)
 
   describe '#init' do
@@ -10,10 +10,10 @@ RSpec.describe NightReader do
       expect(night_reader).to be_a NightReader
     end
 
-    it 'has @file_handler, @dictionary, @array_from_parsed_braille, @message, @characters_in_message, @message_container, @input_data' do
+    it 'has @file_handler, @dictionary, @array_of_braille_pairs, @message, @characters_in_message, @message_container, @input_data' do
       expect(night_reader.file_handler).to be_a FileProcessor
       expect(night_reader.dictionary).to be_a Dictionary
-      expect(night_reader.array_from_parsed_braille).to eq(nil)
+      expect(night_reader.array_of_braille_pairs).to eq(nil)
       expect(night_reader.message).to eq('')
       expect(night_reader.characters_in_message).to eq(0)
       expect(night_reader.message_container).to eq([])
@@ -33,7 +33,7 @@ RSpec.describe NightReader do
 
   describe '#character_containers_needed' do
     it 'measures the english characters neeeded in the message, counting 6 braille characters at a time' do
-      night_reader.array_from_parsed_braille = [['0'], ['.'], ['.'], ['.'], ['.'], ['.']]
+      night_reader.array_of_braille_pairs = [['0'], ['.'], ['.'], ['.'], ['.'], ['.']]
       night_reader.character_containers_needed
       expect(night_reader.characters_in_message).to eq(1)
     end
@@ -41,7 +41,7 @@ RSpec.describe NightReader do
 
   describe '#create_character_containers' do
     it 'adds empty arrays to the @message_container, to represent a letter in the message, which will be populated with braille characters' do
-      night_reader.array_from_parsed_braille = [['0'], ['.'], ['.'], ['.'], ['.'], ['.']]
+      night_reader.array_of_braille_pairs = [['0'], ['.'], ['.'], ['.'], ['.'], ['.']]
       expect(night_reader.characters_in_message).to eq(1)
       night_reader.create_character_containers
       expect(night_reader.message_container).to eq([[]])
@@ -57,9 +57,9 @@ RSpec.describe NightReader do
 
   describe '#fill_character_containers' do
     xit 'cycles through the @message_container and populates each character_container with the braille data' do
-      night_reader.array_from_parsed_braille = [['0'], ['.'], ['.'], ['.'], ['.'], ['.']]
+      night_reader.array_of_braille_pairs = [['0'], ['.'], ['.'], ['.'], ['.'], ['.']]
       night_reader.character_containers_needed
-      raw_braille_pairs = night_reader.array_from_parsed_braille.slice!(0, 120)
+      raw_braille_pairs = night_reader.array_of_braille_pairs.slice!(0, 120)
       night_reader.fill_character_containers(raw_braille_pairs)
       expect(night_reader.message_container).to eq(4)
     end
